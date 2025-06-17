e58020e91c16e9310359f5f6b8b936a25a36a325
bing <spiralladder@keemail.me>
2025-06-17 12:02:17 +0200

    remove go comments

--- src/bench.zig
+++ src/bench.zig
@@ -70,22 +70,3 @@ fn verify() !void {
 pub fn main() !void {
     try verify();
 }
-
-//func benchCommit(scale uint8, b *testing.B) {
-//	fs := NewFFTSettings(scale)
-//	setupG1, setupG2 := GenerateTestingSetup("1234", uint64(1)<<scale)
-//	ks := NewKZGSettings(fs, setupG1, setupG2)
-//	setupLagrange, err := ks.FFTG1(setupG1, true)
-//	if err != nil {
-//		b.Fatal(err)
-//	}
-//	blob := make([]bls.Fr, uint64(1)<<scale)
-//	for i := 0; i < len(blob); i++ {
-//		blob[i] = *bls.RandomFr()
-//	}
-//
-//	b.ResetTimer()
-//	for i := 0; i < b.N; i++ {
-//		bls.LinCombG1(setupLagrange, blob)
-//	}
-//}
