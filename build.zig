aea70aa0f15317ea8817b171273441e963c8af93
bing <spiralladder@keemail.me>
2025-06-17 12:02:17 +0200

    clean up build.zig

--- build.zig
+++ build.zig
@@ -1,16 +1,11 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
         .name = "kzigg",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
@@ -18,14 +13,12 @@ pub fn build(b: *std.Build) void {
 
     const bench = b.addExecutable(.{
         .name = "bench",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/bench.zig"),
         .target = target,
         .optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe }),
     });
 
-    bench.addLibraryPath(b.path("src/main.zig"));
+    bench.addLibraryPath(b.path("src"));
     bench.addIncludePath(b.path("blst/bindings"));
     bench.addLibraryPath(b.path("blst"));
     bench.addObjectFile(b.path("blst/libblst.a"));
@@ -35,12 +28,9 @@ pub fn build(b: *std.Build) void {
     lib.addLibraryPath(b.path("blst"));
     lib.addObjectFile(b.path("blst/libblst.a"));
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
     const install_step = b.addInstallArtifact(bench, .{});
-    const build_step = b.step("build bench", "Build benchmarks");
+    const build_step = b.step("bench", "Build benchmarks");
 
     build_step.dependOn(&install_step.step);
     const run_cmd = b.addRunArtifact(bench);
@@ -48,8 +38,6 @@ pub fn build(b: *std.Build) void {
 
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const filter = b.option([]const u8, "test-filter", "Filter for tests");
 
     const main_tests = b.addTest(.{
@@ -65,9 +53,6 @@ pub fn build(b: *std.Build) void {
 
     const run_main_tests = b.addRunArtifact(main_tests);
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build test`
-    // This will evaluate the `test` step rather than the default, which is "install".
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
 }
